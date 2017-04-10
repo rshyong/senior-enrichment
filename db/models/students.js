2c2f2f7bcdae339d06fabebc31763105dddc3769
Richard Shyong <rshyong@gmail.com>
2017-04-09 23:27:57 -0400

    starting senior enrichment

---
+++ db/models/students.js
@@ -0,0 +1,15 @@
+'use strict';
+const Sequelize = require('sequelize');
+const db = require('../index.js');
+
+module.exports = db.define('students', {
+    name: Sequelize.STRING,
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            isEmail: true,
+            notEmpty: true
+        }
+    },
+    campus: Sequelize.STRING
+})
--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,6 @@
 var Sequelize = require('sequelize')
 var db = require('../index.js')
 
-
 module.exports = db.define('user', {
   name: Sequelize.STRING,
 })
